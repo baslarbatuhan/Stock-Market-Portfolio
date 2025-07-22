fcbfc41476a59b3bf327333901b2dd6517cab40a
Batuhan Baslar <batuhanbaslar@gmail.com>
2025-07-22 18:00:17 +0300

    db & controller & mapper & dto

--- api/Program.cs
+++ api/Program.cs
@@ -1,10 +1,18 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Swagger servisleri
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext <ApplicationDBContext> (options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -15,6 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
+app.MapControllers();
 
 app.Run();
 
